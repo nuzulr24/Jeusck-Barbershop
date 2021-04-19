c84823b3efed554667d7b82b560c620095d7f7c5
Juju Codes <40730150+nuzulr24@users.noreply.github.com>
2021-04-19 14:15:11 +0700

    update final
    final

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -22,7 +22,7 @@ function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("page").style.display = "block";
     document.getElementById("bottom-navigation").style.display = "block";
-    var haha =document.getElementById("breadcrumb").style.display = "block";
+    var haha = document.getElementById("breadcrumb").style.display = "block";
     console.log(haha);
 }
 
@@ -37,3 +37,48 @@ $(".carousel").swipe({
     allowPageScroll: "vertical"
 
 });
+
+// setting maping
+var map = L.map('map');
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    id: 'mapbox.streets'
+}).addTo(map);
+
+// placeholders for the L.marker and L.circle representing user's current position and accuracy    
+var current_position, current_accuracy;
+
+function onLocationFound(e) {
+    // if position defined, then remove the existing position marker and accuracy circle from the map
+    if (current_position) {
+        map.removeLayer(current_position);
+        map.removeLayer(current_accuracy);
+    }
+
+    var radius = e.accuracy / 2;
+
+    current_position = L.marker(e.latlng).addTo(map)
+        .bindPopup("Kamu berada " + radius + " meter dari titik utama").openPopup();
+
+    current_accuracy = L.circle(e.latlng, radius).addTo(map);
+}
+
+function onLocationError(e) {
+    alert(e.message);
+}
+
+map.on('locationfound', onLocationFound);
+map.on('locationerror', onLocationError);
+
+// wrap map.locate in a function    
+function locate() {
+    map.locate({
+        setView: true,
+        maxZoom: 16
+    });
+}
+
+// call locate every 3 seconds... forever
+setInterval(locate, 3000);
